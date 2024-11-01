0504c8f8820d455720b6defcc6fba214c800bc73
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-01 21:32:43 +0300

    fix: image path error in students

--- api/interfaces/students_interface.py
+++ api/interfaces/students_interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from database.database_manager import DatabaseManager
+from PIL import Image, ImageTk
 
 class StudentsInterface:
     def __init__(self, root, show_main_callback):
@@ -40,7 +41,13 @@ class StudentsInterface:
             student_frame = tk.Frame(self.students_list_frame, bd=2, relief="solid")
             student_frame.pack(pady=5, padx=5, fill="x")
 
-            img = tk.PhotoImage(file=image_path)  # Assuming image_path is the path to the image
+            try:
+                img = Image.open(image_path)
+                img = img.resize((100, 100))  # تغيير حجم الصورة حسب الحاجة
+                img = ImageTk.PhotoImage(img)
+            except Exception as e:
+                print(f"Failed to load image {image_path}: {e}")
+                img = None  # تعيين None في حالة فشل التحميل
             img_label = tk.Label(student_frame, image=img)
             img_label.image = img  # Keep a reference to avoid garbage collection
             img_label.pack(side="left", padx=10)
