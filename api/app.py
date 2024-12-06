57593be93b3e778b76f1c04995ef5235d7c6d1a2
abodradan <abdulrahmanraadan@gmail.com>
2024-12-06 20:37:34 +0300

    update the hach data

--- api/app.py
+++ api/app.py
@@ -2,13 +2,21 @@ from services.student_data_handler import StudentDataHandler
 from database.database_manager import DatabaseManager
 
 if __name__ == '__main__':
-    # db_manager = DatabaseManager()
-    # # db_manager.close()
-    # db_manager.connect()
-    # db_manager.create_tables()
+    db_manager = DatabaseManager()
+    # db_manager.close()
+    db_manager.connect()
+    db_manager.create_tables()
 
-    handler = StudentDataHandler()
-    print('studentHandler', handler.generate_student_id())
+    # db_manager.exam_db.create(Date="2024-12-06", TimeSlot="10:00 - 12:00", Period="First", Level="2", Specialization="AI", Duration="02:00:00", StudentCount=50)
+    # db_manager.exam_db.create(Date="2024-12-07", TimeSlot="10:00 - 12:00", Period="First", Level="4", Specialization="CS", Duration="02:00:00", StudentCount=50)
+    # db_manager.exam_db.create(Date="2024-12-08", TimeSlot="10:00 - 12:00", Period="First", Level="3", Specialization="CS", Duration="02:00:00", StudentCount=50)
+    # db_manager.exam_db.create(Date="2024-12-09", TimeSlot="10:00 - 12:00", Period="First", Level="2", Specialization="CS", Duration="02:00:00", StudentCount=50)
+    # db_manager.exam_db.create(Date="2024-12-10", TimeSlot="10:00 - 12:00", Period="First", Level="1", Specialization="CS", Duration="02:00:00", StudentCount=50)
+    db_manager.exam_db.create(Date="2024-12-06", TimeSlot="12:00 - 02:00", Period="Second", Level="4", Specialization="CS", Duration="02:00:00", StudentCount=50)
+
+    # handler = StudentDataHandler()
+    print('db_manager', db_manager.exam_db.all())
+    db_manager.close()
     # student_data = {
     #     "StudentName": "abod",
     #     "number": "22160033",
@@ -28,6 +36,6 @@ if __name__ == '__main__':
     #     "ImagePath": "/images/ahmed.jpg"
     # }
     # handler.save_student(student_data1)
-    print('studentHandler', handler.generate_student_id())
-    is_exists = handler.is_exists('22160029')
-    print('is_exists', is_exists)
+    # print('studentHandler', handler.generate_student_id())
+    # is_exists = handler.is_exists('22160029')
+    # print('is_exists', is_exists)
